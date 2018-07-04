c349a44d818c6969ecd66b29b0353d47b96736a3
jimmiezhang <wengeezhang@gmail.com>
2018-07-04 14:54:34 +0800

    add question answer模块

---
+++ src/Answer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Answer extends Component {
+  render() {
+    return (
+      <div className="Answer">
+        <p className="Answer-desc">
+          答案：go to reactjs.org
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Answer;
---
+++ src/Question.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Question extends Component {
+  render() {
+    return (
+      <div className="Question">
+        <p className="Question-desc">
+          如何学习react
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Question;
