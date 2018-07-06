0f53ec894a578c03af3392bc9eade5783b9faf67
jimmiezhang <wengeezhang@gmail.com>
2018-07-06 15:11:20 +0800

    异步加载远程数据的方式

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import Answer from './Answer';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      answers: []
+    };
+  }
+  componentDidMount(){
+    // fetch().then() 加载远端接口真实数据
+    this.setState({answers: ["go to ReactJs.org!","learn from forum"]})
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +21,7 @@ class App extends Component {
           <h1 className="App-title">付费知识</h1>
         </header>
         <Question />
-        <Answer>go to Reactjs.org!!!</Answer>
+        {this.state.answers.map(answer => <Answer>{answer}</Answer>)}
       </div>
     );
   }
